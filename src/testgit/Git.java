eacc1165f436bbff15cfbabecdb012872347fc1b
ariicas <ariicastrobustoss@gmail.com>
2021-03-11 20:29:16 -0500

    new file git added

---
+++ src/testgit/Git.java
@@ -0,0 +1,16 @@
+package testgit;
+
+public class Git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=1;
+		int num2=27;
+		int sum=0;
+		
+		sum=num+num2;
+		
+		System.out.println("My name is Ariana and I am "+sum);
+	}
+
+}
